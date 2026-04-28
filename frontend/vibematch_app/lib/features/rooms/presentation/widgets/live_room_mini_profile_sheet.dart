1643293df387bff4481b6b314287a016f1161a3e
Sree Harsha Reddy <sreeharshareddyyeddula@gmail.com>
2026-04-28 22:14:50 +0530

    Move mini profile and settings wiring into modules

---
+++ frontend/vibematch_app/lib/features/rooms/presentation/widgets/live_room_mini_profile_sheet.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+import '../live_room_models.dart';
+import 'room_profile_sheet.dart';
+
+class LiveRoomMiniProfileSheet extends StatelessWidget {
+  const LiveRoomMiniProfileSheet({
+    super.key,
+    required this.user,
+    required this.currentUser,
+    required this.canModerate,
+    required this.onAvatarTap,
+    required this.onVipTap,
+    required this.onSendingLevelTap,
+    required this.onReceivingLevelTap,
+    required this.onSentRankingTap,
+    required this.onReceivedRankingTap,
+    required this.onFamilyTap,
+    required this.onRelationshipTap,
+    required this.onMedalsTap,
+    required this.onMentionTap,
+    required this.onSetAdminTap,
+    required this.onLeaveAndLock,
+    required this.onSelfMuteToggle,
+    required this.onAdminMuteToggle,
+    required this.onGiftTap,
+  });
+
+  final SeatUser user;
+  final SeatUser currentUser;
+  final bool canModerate;
+
+  final VoidCallback onAvatarTap;
+  final VoidCallback onVipTap;
+  final VoidCallback onSendingLevelTap;
+  final VoidCallback onReceivingLevelTap;
+  final VoidCallback onSentRankingTap;
+  final VoidCallback onReceivedRankingTap;
+  final VoidCallback onFamilyTap;
+  final VoidCallback onRelationshipTap;
+  final VoidCallback onMedalsTap;
+  final VoidCallback onMentionTap;
+  final VoidCallback onSetAdminTap;
+  final VoidCallback onLeaveAndLock;
+  final VoidCallback onSelfMuteToggle;
+  final VoidCallback onAdminMuteToggle;
+  final VoidCallback onGiftTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return UserMiniProfileSheet(
+      user: user,
+      currentUser: currentUser,
+      canModerate: canModerate,
+      onAvatarTap: onAvatarTap,
+      onVipTap: onVipTap,
+      onSendingLevelTap: onSendingLevelTap,
+      onReceivingLevelTap: onReceivingLevelTap,
+      onSentRankingTap: onSentRankingTap,
+      onReceivedRankingTap: onReceivedRankingTap,
+      onFamilyTap: onFamilyTap,
+      onRelationshipTap: onRelationshipTap,
+      onMedalsTap: onMedalsTap,
+      onMentionTap: onMentionTap,
+      onSetAdminTap: onSetAdminTap,
+      onLeaveAndLock: onLeaveAndLock,
+      onSelfMuteToggle: onSelfMuteToggle,
+      onAdminMuteToggle: onAdminMuteToggle,
+      onGiftTap: onGiftTap,
+    );
+  }
+}
