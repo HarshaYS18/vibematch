275b275455d2cbe2078c75ad1b3e3072ee40e6a5
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-04-28 23:15:16 +0530

    Silence bottom snackbar popups globally

--- frontend/vibematch_app/lib/main.dart
+++ frontend/vibematch_app/lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'features/auth/presentation/auth_gate.dart';
 
+final GlobalKey<ScaffoldMessengerState> rootScaffoldMessengerKey =
+    GlobalKey<ScaffoldMessengerState>();
+
 void main() {
   runApp(const VibeMatchApp());
 }
@@ -14,6 +17,7 @@ class VibeMatchApp extends StatelessWidget {
     return MaterialApp(
       title: 'Vibe Match',
       debugShowCheckedModeBanner: false,
+      scaffoldMessengerKey: rootScaffoldMessengerKey,
       theme: ThemeData(
         useMaterial3: true,
         fontFamily: 'Roboto',
@@ -23,7 +27,34 @@ class VibeMatchApp extends StatelessWidget {
         ),
         scaffoldBackgroundColor: const Color(0xFFFAF7F1),
       ),
+      builder: (context, child) {
+        return _BottomPopupSilencer(child: child ?? const SizedBox.shrink());
+      },
       home: const AuthGate(),
     );
   }
+}
+
+class _BottomPopupSilencer extends StatefulWidget {
+  const _BottomPopupSilencer({required this.child});
+
+  final Widget child;
+
+  @override
+  State<_BottomPopupSilencer> createState() => _BottomPopupSilencerState();
+}
+
+class _BottomPopupSilencerState extends State<_BottomPopupSilencer> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPersistentFrameCallback((_) {
+      final messenger = rootScaffoldMessengerKey.currentState;
+      messenger?.clearSnackBars();
+      messenger?.clearMaterialBanners();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.child;
 }
