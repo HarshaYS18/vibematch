caef24b6f0655e5ca6b5c33a9415eda7440811d1
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-04-29 10:39:19 +0530

    Use modular Home page in app shell

--- frontend/vibematch_app/lib/app/app_shell.dart
+++ frontend/vibematch_app/lib/app/app_shell.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../features/auth/models/current_user.dart';
 import '../features/create/presentation/create_page.dart';
-import '../features/home/presentation/home_page.dart';
+import '../features/home/presentation/home_page_modular.dart';
 import '../features/inbox/presentation/inbox_page.dart';
 import '../features/profile/presentation/me_page.dart';
 import '../features/rooms/presentation/widgets/live_room_minimized_bubble.dart';
