1857891d57d541d5ab30465d60fe4fbd01969cac
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-04-29 11:09:21 +0530

    Export Room model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -5,6 +5,7 @@ from app.models.admin_log import AdminLog
 from app.models.special_permission import SpecialPermission, SpecialPermissionName
 from app.models.user_ban import UserBan, BanType, BanSource
 from app.models.device_ban import DeviceBan
+from app.models.room import Room, RoomMode, RoomType
 from app.models.login_history import (
     LoginHistory,
     LoginHistoryStatus,
