f892f0f3eed7bf00eb7a32221d4a88436ad97a22
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-05-01 19:18:33 +0530

    Add Vibes comment model

---
+++ frontend/vibematch_app/lib/features/vibes/models/vibe_comment.dart
@@ -0,0 +1,13 @@
+class VibeComment {
+  const VibeComment({
+    required this.name,
+    required this.avatarText,
+    required this.text,
+    required this.time,
+  });
+
+  final String name;
+  final String avatarText;
+  final String text;
+  final String time;
+}
