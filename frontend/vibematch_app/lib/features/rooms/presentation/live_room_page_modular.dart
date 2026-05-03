06a2b1e9a422a271a81c8a72f9c793e1fa5bceae
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-05-04 02:28:55 +0530

    Make modular live room the canonical class

--- frontend/vibematch_app/lib/features/rooms/presentation/live_room_page_modular.dart
+++ frontend/vibematch_app/lib/features/rooms/presentation/live_room_page_modular.dart
@@ -1,1 +1,12 @@
-export 'live_room_page_backend_safe.dart' show LiveRoomPageBackendSafe;
+import 'live_room_page_backend_safe.dart';
+
+class LiveRoomPageModular extends LiveRoomPageBackendSafe {
+  const LiveRoomPageModular({
+    super.key,
+    super.roomName,
+    super.roomId,
+    super.language,
+    super.modeTitle,
+    super.onlineCount,
+  });
+}
