f3194dcea7703679ab6d9834f8857a1c7cb59df8
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-05-04 05:29:23 +0530

    Add home room shortcut module

---
+++ frontend/vibematch_app/lib/features/home/presentation/widgets/home_room_shortcut.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../../models/home_room_data.dart';
+
+class HomeRoomShortcut extends StatelessWidget {
+  const HomeRoomShortcut({
+    super.key,
+    required this.room,
+    required this.onTap,
+  });
+
+  final HomeRoomData? room;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final hasRoom = room != null;
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 52,
+        height: 52,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(18),
+          border: Border.all(color: const Color(0xFFEDE3D7)),
+          boxShadow: [
+            BoxShadow(
+              color: const Color(0xFF251538).withValues(alpha: 0.05),
+              blurRadius: 16,
+              offset: const Offset(0, 8),
+            ),
+          ],
+        ),
+        child: Icon(
+          hasRoom ? Icons.home_rounded : Icons.add_home_work_rounded,
+          color: hasRoom ? const Color(0xFF12C7B7) : const Color(0xFF251538),
+          size: 24,
+        ),
+      ),
+    );
+  }
+}
