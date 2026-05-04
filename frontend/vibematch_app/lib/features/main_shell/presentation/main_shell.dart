28ebf8fb8d431a459640fd40caf6a22f18647187
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-05-04 05:36:05 +0530

    Route main shell to modular home page

--- frontend/vibematch_app/lib/features/main_shell/presentation/main_shell.dart
+++ frontend/vibematch_app/lib/features/main_shell/presentation/main_shell.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../auth/models/current_user.dart';
 import '../../create/presentation/create_page.dart';
-import '../../home/presentation/home_page.dart';
+import '../../home/presentation/home_page_modular.dart';
 import '../../inbox/presentation/inbox_page.dart';
 import '../../profile/presentation/me_page.dart';
 import '../../vibes/presentation/vibes_page.dart';
