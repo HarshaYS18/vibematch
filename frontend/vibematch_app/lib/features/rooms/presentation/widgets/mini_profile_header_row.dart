673accc89542b9b30482eead51fb4abc947ce203
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-05-04 13:23:26 +0530

    Modularize mini profile header row

--- frontend/vibematch_app/lib/features/rooms/presentation/widgets/mini_profile_header_row.dart
+++ frontend/vibematch_app/lib/features/rooms/presentation/widgets/mini_profile_header_row.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../live_room_models.dart';
 import 'mini_profile_decoration.dart';
+import 'mini_profile_header/mini_profile_more_menu_button.dart';
+import 'mini_profile_header/mini_profile_report_icon_button.dart';
 import 'room_theme.dart';
 
 class MiniProfileHeaderRow extends StatelessWidget {
@@ -34,13 +36,13 @@ class MiniProfileHeaderRow extends StatelessWidget {
           Align(
             alignment: Alignment.centerLeft,
             child: showAdminMenu
-                ? _MiniProfileMoreMenuButton(
+                ? MiniProfileMoreMenuButton(
                     user: user,
                     onSetAdminTap: onSetAdminTap,
                     onRemoveAdminTap: onRemoveAdminTap,
                     onReportTap: onReportTap,
                   )
-                : _MiniProfileReportIconButton(onTap: onReportTap),
+                : MiniProfileReportIconButton(onTap: onReportTap),
           ),
           Center(
             child: Padding(
@@ -72,116 +74,6 @@ class MiniProfileHeaderRow extends StatelessWidget {
             ),
         ],
       ),
-    );
-  }
-}
-
-class _MiniProfileReportIconButton extends StatelessWidget {
-  const _MiniProfileReportIconButton({required this.onTap});
-
-  final VoidCallback onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return MiniProfileCornerButton(
-      icon: Icons.report_gmailerrorred_rounded,
-      color: RoomColors.coral,
-      size: 32,
-      iconSize: 17,
-      onTap: onTap,
-    );
-  }
-}
-
-class _MiniProfileMoreMenuButton extends StatelessWidget {
-  const _MiniProfileMoreMenuButton({
-    required this.user,
-    required this.onSetAdminTap,
-    required this.onRemoveAdminTap,
-    required this.onReportTap,
-  });
-
-  final SeatUser user;
-  final VoidCallback onSetAdminTap;
-  final VoidCallback onRemoveAdminTap;
-  final VoidCallback onReportTap;
-
-  @override
-  Widget build(BuildContext context) {
-    final canRemoveAdmin = user.isRoomAdmin && !user.isHost;
-
-    return PopupMenuButton<String>(
-      tooltip: 'More',
-      offset: const Offset(0, 34),
-      color: Colors.white,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
-      onSelected: (value) {
-        if (value == 'set_admin') onSetAdminTap();
-        if (value == 'remove_admin') onRemoveAdminTap();
-        if (value == 'report') onReportTap();
-      },
-      itemBuilder: (context) => [
-        if (canRemoveAdmin)
-          const PopupMenuItem<String>(
-            value: 'remove_admin',
-            child: _MiniProfileMenuRow(
-              icon: Icons.shield_moon_rounded,
-              color: RoomColors.coral,
-              label: 'Remove admin',
-            ),
-          )
-        else if (!user.isRoomAdmin && !user.isHost)
-          const PopupMenuItem<String>(
-            value: 'set_admin',
-            child: _MiniProfileMenuRow(
-              icon: Icons.shield_rounded,
-              color: RoomColors.aqua,
-              label: 'Set as admin',
-            ),
-          ),
-        const PopupMenuItem<String>(
-          value: 'report',
-          child: _MiniProfileMenuRow(
-            icon: Icons.report_gmailerrorred_rounded,
-            color: RoomColors.coral,
-            label: 'Report',
-          ),
-        ),
-      ],
-      child: Container(
-        width: 32,
-        height: 32,
-        decoration: BoxDecoration(
-          color: RoomColors.plum.withValues(alpha: 0.07),
-          shape: BoxShape.circle,
-        ),
-        child: const Icon(Icons.more_horiz_rounded, color: RoomColors.plum, size: 20),
-      ),
-    );
-  }
-}
-
-class _MiniProfileMenuRow extends StatelessWidget {
-  const _MiniProfileMenuRow({required this.icon, required this.color, required this.label});
-
-  final IconData icon;
-  final Color color;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(icon, size: 18, color: color),
-        const SizedBox(width: 9),
-        Text(
-          label,
-          style: const TextStyle(
-            color: RoomColors.plum,
-            fontWeight: FontWeight.w800,
-          ),
-        ),
-      ],
     );
   }
 }
