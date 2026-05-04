4bfdb1f768b7eaf92f56eb6c972d077713d8de17
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-05-04 10:52:55 +0530

    Remove dropped room preview route factory references

--- frontend/vibematch_app/lib/app/app_route_factory.dart
+++ frontend/vibematch_app/lib/app/app_route_factory.dart
@@ -11,7 +11,6 @@ import '../features/notifications/presentation/notifications_page.dart';
 import '../features/profile/presentation/public_profile_page.dart';
 import '../features/rankings/presentation/rankings_page.dart';
 import '../features/rooms/presentation/live_room_page.dart';
-import '../features/rooms/presentation/room_preview_page.dart';
 import '../features/rooms/presentation/routes/live_room_route_args.dart';
 import '../features/rooms/presentation/routes/live_room_routes.dart';
 import '../features/search/presentation/search_page.dart';
@@ -43,22 +42,6 @@ class AppRouteFactory {
           );
         }
         return _buildRoute(settings, const LiveRoomPage());
-
-      case VmRoutes.roomPreview:
-        final args = settings.arguments;
-        if (args is RoomPreviewRouteArgs) {
-          return _buildRoute(
-            settings,
-            RoomPreviewPage(
-              roomName: args.roomName,
-              roomId: args.roomId,
-              language: args.language,
-              modeTitle: args.modeTitle,
-              onlineCount: args.onlineCount,
-            ),
-          );
-        }
-        return _buildRoute(settings, const RoomPreviewPage());
 
       case VmRoutes.profile:
         final args = settings.arguments;
