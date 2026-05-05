3697d9d37dc0d0027255a0cfc9350934a4ff97e2
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-05-06 02:19:46 +0530

    Add public profile widget barrel

---
+++ frontend/vibematch_app/lib/features/profile/presentation/widgets/public_profile_widgets.dart
@@ -0,0 +1,5 @@
+export 'public_profile_access_dialog.dart';
+export 'public_profile_header.dart';
+export 'public_profile_panels.dart';
+export 'public_profile_shared_widgets.dart';
+export 'public_profile_vibe_card.dart';
