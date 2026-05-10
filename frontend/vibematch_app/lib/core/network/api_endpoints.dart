69f6012bf240017930fa8c3ea05351ad83d773d1
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-05-11 02:38:28 +0530

    Add following rooms endpoint constant

--- frontend/vibematch_app/lib/core/network/api_endpoints.dart
+++ frontend/vibematch_app/lib/core/network/api_endpoints.dart
@@ -4,4 +4,5 @@ class ApiEndpoints {
   static const String localBaseUrl = 'http://127.0.0.1:8000';
 
   static const String roomsTrending = '/rooms/trending';
+  static const String roomsFollowing = '/rooms/following';
 }
