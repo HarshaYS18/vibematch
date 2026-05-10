e9051829920894c87483b8e5bb3f570a2a95dd0f
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-05-11 03:07:27 +0530

    Open backend game test from live room games sheet

--- frontend/vibematch_app/lib/features/rooms/presentation/modules/live_room_games_actions_module.dart
+++ frontend/vibematch_app/lib/features/rooms/presentation/modules/live_room_games_actions_module.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../games/presentation/game_test_page.dart';
 import '../controllers/live_room_sheet_controller.dart';
 import '../widgets/live_room_games_sheet.dart';
 import '../widgets/room_theme.dart';
@@ -17,19 +18,19 @@ class LiveRoomGamesActionsModule {
       builder: (_) => LiveRoomGamesSheet(
         onCrystalHuntTap: () {
           Navigator.pop(context);
-          RoomToast.show(context, 'Crystal Hunt opens here');
+          Navigator.of(context).push(MaterialPageRoute(builder: (_) => const GameTestPage()));
         },
         onLudoTap: () {
           Navigator.pop(context);
-          RoomToast.show(context, 'Ludo opens here');
+          RoomToast.show(context, 'Ludo will connect to backend game catalog after free-game module is added.');
         },
         onCarromTap: () {
           Navigator.pop(context);
-          RoomToast.show(context, 'Carrom opens here');
+          RoomToast.show(context, 'Carrom will connect to backend game catalog after free-game module is added.');
         },
         onPkTap: () {
           Navigator.pop(context);
-          RoomToast.show(context, 'PK game opens here');
+          RoomToast.show(context, 'PK game will connect to backend room battle flow after PK backend is added.');
         },
       ),
     );
