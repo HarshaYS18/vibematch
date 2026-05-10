e6f9ee7d75131a54d2f5ce9d997cf8ea211261bb
Sree Harsha Reddy <sreeharshareddyyeddula@gmail.com>
2026-05-11 02:56:58 +0530

    Save local clean Flutter state

--- frontend/vibematch_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/vibematch_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import device_info_plus
 import file_picker
 import file_selector_macos
 import flutter_webrtc
+import google_sign_in_ios
 import mobile_scanner
 import shared_preferences_foundation
 import video_player_avfoundation
@@ -18,6 +19,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
