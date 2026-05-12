ce92626571e48792e5a8e15e63c49dd677031022
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-05-12 18:32:20 +0530

    Merge cricket and profile Alembic heads

---
+++ backend/alembic/versions/c9d0e1f2a345_merge_cricket_and_profile_heads.py
@@ -0,0 +1,20 @@
+"""merge cricket and profile heads
+
+Revision ID: c9d0e1f2a345
+Revises: b7c8d9e0f123, 68ce5961f53c
+Create Date: 2026-05-12 18:45:00.000000
+"""
+
+
+revision = "c9d0e1f2a345"
+down_revision = ("b7c8d9e0f123", "68ce5961f53c")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
