e18b14f686650a6dfbab41bd0e5fd8c3ce6efdad
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-05-12 18:36:10 +0530

    Merge room custom and cricket Alembic heads

---
+++ backend/alembic/versions/d1e2f3a4b567_merge_room_custom_and_cricket_heads.py
@@ -0,0 +1,20 @@
+"""merge room custom and cricket heads
+
+Revision ID: d1e2f3a4b567
+Revises: c9d0e1f2a345, 20260512_room_custom
+Create Date: 2026-05-12 18:55:00.000000
+"""
+
+
+revision = "d1e2f3a4b567"
+down_revision = ("c9d0e1f2a345", "20260512_room_custom")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
