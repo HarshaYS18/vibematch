c5dabda67a923eb211cb13d19548d8f241721358
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-05-14 12:34:09 +0530

    Import material types in gift controller

--- frontend/vibematch_app/lib/features/rooms/presentation/controllers/live_room_gift_controller.dart
+++ frontend/vibematch_app/lib/features/rooms/presentation/controllers/live_room_gift_controller.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart' show Alignment, Color;
 
 import '../../../wallet/data/wallet_api_service.dart';
 import '../../data/active_room_context.dart';
