338e319574e2bffec8648bd7f149bb6359f3b8f8
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-05-22 11:11:27 +0530

    Enable Android core library desugaring

--- frontend/vibematch_app/android/app/build.gradle.kts
+++ frontend/vibematch_app/android/app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -23,10 +24,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.funkey.app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -46,4 +44,6 @@ flutter {
     source = "../.."
 }
 
-
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+}
