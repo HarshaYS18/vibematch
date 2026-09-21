b7035c27dd7d5bd7b6eb2968d0e177e20db9f69c
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-09-21 19:16:48 +0530

    Merge legacy Alembic heads

---
+++ backend/alembic/versions/20260921_0100_merge_legacy_heads.py
@@ -0,0 +1,19 @@
+"""merge disconnected legacy migration heads
+
+Revision ID: 20260921_0100
+Revises: 20260913_0100, 20260513_0100
+Create Date: 2026-09-21
+"""
+
+revision = "20260921_0100"
+down_revision = ("20260913_0100", "20260513_0100")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
