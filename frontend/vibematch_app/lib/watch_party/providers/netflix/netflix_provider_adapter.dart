215a08f657bb1b6885c5820589e8c32f88ab3d34
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-09-23 01:21:20 +0530

    feat(watch-party): add capability-driven OTT adapters

---
+++ frontend/vibematch_app/lib/watch_party/providers/netflix/netflix_provider_adapter.dart
@@ -0,0 +1,24 @@
+import '../../../foundation/telemetry/app_telemetry.dart';
+import '../companion/companion_playback_adapter.dart';
+import '../web/ott_provider_definition.dart';
+import '../web/ott_web_playback_host.dart';
+import '../web/supported_web_playback_adapter.dart';
+
+class NetflixProviderAdapter extends SupportedWebPlaybackAdapter {
+  NetflixProviderAdapter({
+    OttWebPlaybackHost? host,
+    CompanionPlaybackAdapter? companion,
+    AppTelemetry telemetry = const AppTelemetry(),
+  }) : super(
+         provider: OttProviderCatalog.netflix,
+         host:
+             host ??
+             InAppWebViewOttPlaybackHost(
+               provider: OttProviderCatalog.netflix,
+             ),
+         companion:
+             companion ??
+             CompanionPlaybackAdapter(provider: OttProviderCatalog.netflix),
+         telemetry: telemetry,
+       );
+}
