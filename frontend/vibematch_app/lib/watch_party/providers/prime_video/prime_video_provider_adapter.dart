a9366f449a19ecde707fa54796c9058edfeba5f0
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-09-23 01:27:13 +0530

    feat(watch-party): add Prime Video OTT adapter

---
+++ frontend/vibematch_app/lib/watch_party/providers/prime_video/prime_video_provider_adapter.dart
@@ -0,0 +1,24 @@
+import '../../../foundation/telemetry/app_telemetry.dart';
+import '../companion/companion_playback_adapter.dart';
+import '../web/ott_provider_definition.dart';
+import '../web/ott_web_playback_host.dart';
+import '../web/supported_web_playback_adapter.dart';
+
+class PrimeVideoProviderAdapter extends SupportedWebPlaybackAdapter {
+  PrimeVideoProviderAdapter({
+    OttWebPlaybackHost? host,
+    CompanionPlaybackAdapter? companion,
+    AppTelemetry telemetry = const AppTelemetry(),
+  }) : super(
+         provider: OttProviderCatalog.primeVideo,
+         host:
+             host ??
+             InAppWebViewOttPlaybackHost(
+               provider: OttProviderCatalog.primeVideo,
+             ),
+         companion:
+             companion ??
+             CompanionPlaybackAdapter(provider: OttProviderCatalog.primeVideo),
+         telemetry: telemetry,
+       );
+}
