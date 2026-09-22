b2dca075272427b0d591b91616a96efac2daf506
HarshaYS18 <47577856+HarshaYS18@users.noreply.github.com>
2026-09-23 01:27:15 +0530

    feat(watch-party): add JioHotstar OTT adapter

---
+++ frontend/vibematch_app/lib/watch_party/providers/jiohotstar/jiohotstar_provider_adapter.dart
@@ -0,0 +1,24 @@
+import '../../../foundation/telemetry/app_telemetry.dart';
+import '../companion/companion_playback_adapter.dart';
+import '../web/ott_provider_definition.dart';
+import '../web/ott_web_playback_host.dart';
+import '../web/supported_web_playback_adapter.dart';
+
+class JioHotstarProviderAdapter extends SupportedWebPlaybackAdapter {
+  JioHotstarProviderAdapter({
+    OttWebPlaybackHost? host,
+    CompanionPlaybackAdapter? companion,
+    AppTelemetry telemetry = const AppTelemetry(),
+  }) : super(
+         provider: OttProviderCatalog.jioHotstar,
+         host:
+             host ??
+             InAppWebViewOttPlaybackHost(
+               provider: OttProviderCatalog.jioHotstar,
+             ),
+         companion:
+             companion ??
+             CompanionPlaybackAdapter(provider: OttProviderCatalog.jioHotstar),
+         telemetry: telemetry,
+       );
+}
